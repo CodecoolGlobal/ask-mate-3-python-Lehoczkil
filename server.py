f0ca881803fe50491fa2560a56f287131d07c386
Lehoczki László Péter <lehoczkilaszlo2002@gmail.com>
2022-03-21 13:56:33 +0100

    add skeleton code

--- server.py
+++ server.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/list')
+def list_page():
+    return render_template('list.html')
+
+
+@app.route('/question/<question_id>')
+def questions_page():
+    return render_template('questions.html')
+
+
+@app.route('/add-question')
+def add_question():
+    return render_template('add_question.html')
+
+
+@app.route('/question/<question_id>/new-answer')
+def post_answer():
+    return render_template('post_answer.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=True,
+        port=5000
+    )
