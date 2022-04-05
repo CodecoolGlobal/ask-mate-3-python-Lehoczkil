6145d4bd0d596b0be98fb622d64705cddcc9ee1a
Kozma Gábor <kozma.jr@gmail.com>
2022-04-05 14:34:22 +0200

    Add update function for updating questions with SQL.

--- data_handler.py
+++ data_handler.py
@@ -67,16 +67,14 @@ def add_answer(cursor, submission_time, question_id, message, image):
         VALUES ('{submission_time}', 0, {question_id}, '{message}', '{image}');""")
 
 
-def update_line(filename, line):
-    with open(filename, 'w', newline='') as f:
-        if filename == 'sample_data/answer.csv':
-            fieldnames = ANSWER_HEADER
-        elif filename == 'sample_data/question.csv':
-            fieldnames = QUESTION_HEADER
-        writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
-        writer.writeheader()
-        for data in line:
-            writer.writerow(data)
+@database_common.connection_handler
+def update_question(cursor, question_id, updated_title, updated_message):
+    query = f"""
+        UPDATE question 
+        SET  title = '{updated_title}', message = '{updated_message}'
+        WHERE id = '{question_id}'
+        """
+    cursor.execute(query)
 
 
 def convert_date():
