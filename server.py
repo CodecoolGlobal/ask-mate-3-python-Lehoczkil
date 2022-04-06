f2602eebcdb492186b8a1ff8baa4bc25cd802bc6
Suto Gabor <wakacorpz@gmail.com>
2022-04-06 09:47:44 +0200

    Adding edit_answer_comment function to server.py

--- server.py
+++ server.py
@@ -195,6 +195,28 @@ def delete_comment(comment_id):
         return redirect('/answer/<answer_id>')
 
 
+@app.route('/answer/<answer_id>/comment/<comment_id>', methods=['GET', 'POST'])
+def edit_answer_comment(comment_id, answer_id):
+    comment = data_handler.get_comment_data(comment_id)
+    if request.method == 'POST':
+        edited_comment = request.form['message']
+        edited_count = int(request.form['edited_count']) + 1
+        data_handler.edit_comment(edited_comment, comment_id, edited_count)
+        return redirect('answer/<answer_id>')
+    return render_template('edit_comment.html', answer_id=answer_id, comment_id=comment_id, comment=comment)
+
+
+@app.route('/answer/<answer_id>/comment/<comment_id>', methods=['GET', 'POST'])
+def edit_answer_comment(comment_id, answer_id):
+    comment = data_handler.get_comment_data(comment_id)
+    if request.method == 'POST':
+        edited_comment = request.form['message']
+        edited_count = int(request.form['edited_count']) + 1
+        data_handler.edit_comment(edited_comment, comment_id, edited_count)
+        return redirect('answer/<answer_id>')
+    return render_template('edit_comment.html', answer_id=answer_id, comment_id=comment_id, comment=comment)
+
+
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
