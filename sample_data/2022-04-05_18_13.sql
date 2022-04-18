5d3fc1702a471487c243361bcc2618752b09838a
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-04-18 20:56:43 +0200

    Add current time for user and comment tables

--- sample_data/2022-04-05_18_13.sql
+++ sample_data/2022-04-05_18_13.sql
@@ -2,3 +2,7 @@ alter table answer
     alter column submission_time set default current_timestamp;
 alter table question
     alter column submission_time set default current_timestamp;
+alter table comment
+    alter column submission_time set default current_timestamp;
+alter table user
+    alter column submission_time set default current_timestamp;
