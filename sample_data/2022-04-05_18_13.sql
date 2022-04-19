484fc6ddbc00a55d591e4791a9733d90806e78eb
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-04-19 10:33:55 +0200

    Fix default time set for users table

--- sample_data/2022-04-05_18_13.sql
+++ sample_data/2022-04-05_18_13.sql
@@ -4,5 +4,5 @@ alter table question
     alter column submission_time set default current_timestamp;
 alter table comment
     alter column submission_time set default current_timestamp;
-alter table user
-    alter column submission_time set default current_timestamp;
+alter table users
+    alter column registration_time set default current_timestamp;
